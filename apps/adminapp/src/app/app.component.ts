5cef1bbfb4727f3d18e854f1678ef23889a4b418
Gyan Prakash <gyanp008@gmail.com>
2021-11-19 18:30:42 +0530

    updated app component

--- apps/adminapp/src/app/app.component.ts
+++ apps/adminapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nownthenfrontend-root',
+  selector: 'adminapp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
