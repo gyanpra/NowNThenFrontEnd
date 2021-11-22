4d4381cd655eb12d5786903ef5682b72cff68712
Gyan Prakash <gyanp008@gmail.com>
2021-11-22 23:14:33 +0530

    minor exports for app level

--- libs/products/src/index.ts
+++ libs/products/src/index.ts
@@ -1,1 +1,22 @@
+//module export
 export * from './lib/products.module';
+
+
+
+
+
+
+//model export
+export * from './lib/models/categories';
+export * from './lib/models/products';
+
+
+
+
+
+
+
+
+//services export
+export * from './lib/services/categories.service';
+export * from './lib/services/products.service';
