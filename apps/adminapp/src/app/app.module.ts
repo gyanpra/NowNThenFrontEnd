caf5b84ab4d2a4095e47526f571a99e7cf8be733
Gyan Prakash <gyanp008@gmail.com>
2021-11-22 23:09:42 +0530

    added primeng modules

--- apps/adminapp/src/app/app.module.ts
+++ apps/adminapp/src/app/app.module.ts
@@ -1,17 +1,95 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainComponent } from './shared/main/main.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DockModule } from 'primeng/dock';
+import { CategoriesService, ProductsService } from '@nownthenfrontend/products';
+import { AddCategoryComponent } from './pages/categories/add-category/add-category.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+import {ColorPickerModule} from 'primeng/colorpicker';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { AddProductComponent } from './pages/products/add-product/add-product.component';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {DropdownModule} from 'primeng/dropdown';
+import {EditorModule} from 'primeng/editor';
+
+
+
+const primeModules = [
+  CardModule,
+  ToolbarModule,
+  ButtonModule,
+  TableModule,
+  DockModule,
+  InputTextModule,
+  ToastModule,
+  ConfirmDialogModule,
+  ColorPickerModule,
+  InputNumberModule,
+  InputTextareaModule,
+  ToggleButtonModule,
+  InputSwitchModule,
+  DropdownModule,
+  EditorModule
+
+];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'categories', component: CategoriesListComponent },
+      { path: 'categories/add', component: AddCategoryComponent },
+      { path: 'categories/add/:id', component: AddCategoryComponent },
+      { path: 'products', component: ProductsListComponent },
+      { path: 'products/add', component: AddProductComponent },
+      { path: 'products/add/:id', component: AddProductComponent },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    MainComponent,
+    SidenavComponent,
+    CategoriesListComponent,
+    AddCategoryComponent,
+    ProductsListComponent,
+    AddProductComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ...primeModules,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
-  providers: [],
+  providers: [CategoriesService, MessageService,   ConfirmationService, ProductsService],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
