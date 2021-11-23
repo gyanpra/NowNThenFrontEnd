6b346e5896663a3a43ec7ea9554d58e4dd61e98f
Gyan Prakash <gyanp008@gmail.com>
2021-11-24 03:30:07 +0530

    library exports

--- libs/users/src/index.ts
+++ libs/users/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/users.module';
 export * from './lib/models/users';
 export * from './lib/services/users.service';
-
+export * from './lib/services/authguard.guard';
+export * from './lib/services/jwt.interceptor';
+export * from './lib/services/auth.service';
