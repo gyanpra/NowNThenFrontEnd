ac3af66e80c2ae42fe2c1147ff6bfab012a71732
Gyan Prakash <gyanp008@gmail.com>
2021-11-27 14:28:07 +0530

    app stats added

--- apps/adminapp/src/app/pages/dashboard/dashboard.component.ts
+++ apps/adminapp/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '@nownthenfrontend/orders';
+import { ProductsService } from '@nownthenfrontend/products';
+import { User, UsersService } from '@nownthenfrontend/users';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'adminapp-dashboard',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  stats= Array<number>();
+
+  constructor(private usersService: UsersService, private productsService: ProductsService, private ordersService: OrdersService) { }
 
   ngOnInit(): void {
+    combineLatest([this.usersService.getUserCount(),this.productsService.getProductCount(),this.ordersService.getTotalOrders(),this.ordersService.getRevenue()]).subscribe(results => {
+      this.stats = results;
+      console.log(this.stats[3]);
   }
+  );
+}
+
+
+
 
 }
