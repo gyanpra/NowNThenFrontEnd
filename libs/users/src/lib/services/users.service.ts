bcb7d97842136cdf680a5a913231dc7f31c71118
Gyan Prakash <gyanp008@gmail.com>
2021-11-27 14:29:04 +0530

    total users added

--- libs/users/src/lib/services/users.service.ts
+++ libs/users/src/lib/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/users';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from 'environment/environment.prod';
 
 
@@ -34,6 +34,10 @@ export class UsersService {
 
   updateUser(user: User): Observable<User> {
     return this.http.put<User>(`${this.userAPI}/${user.id}`, user);
+  }
+
+  getUserCount(): Observable<number> {
+    return this.http.get<number>(`${this.userAPI}/get/totalcount`).pipe(map((objectValue: any) => objectValue.count));
   }
   
 
