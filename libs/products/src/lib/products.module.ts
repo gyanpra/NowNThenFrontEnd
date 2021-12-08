b19e89332b2e007cd5832d258548d009ee610514
Gyan Prakash <gyanp008@gmail.com>
2021-12-08 17:45:13 +0530

    imports

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { ProductSearchComponent } from './components/product-search/product-search.component';
 import { CategoryBannerComponent } from "./components/category-banner/category-banner.component";
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 import {ButtonModule} from 'primeng/button';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import {CheckboxModule} from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
 
+const routes: Routes = [
+  {path:'products',component:ProductsListComponent},
+  {path:'category/:categoryid',component:ProductsListComponent}
+]
 @NgModule({
-  imports: [CommonModule, RouterModule,ButtonModule],
+  imports: [CommonModule,CheckboxModule, RouterModule.forChild(routes),ButtonModule,FormsModule],
   declarations: [
     ProductSearchComponent,
     CategoryBannerComponent,
     ProductItemComponent,
     FeaturedProductsComponent,
+    ProductsListComponent,
 
   ],
   exports: [
@@ -21,6 +29,7 @@ import {ButtonModule} from 'primeng/button';
     CategoryBannerComponent,
     ProductItemComponent,
     FeaturedProductsComponent,
+    ProductsListComponent,
   ]
 })
 export class ProductsModule {}
