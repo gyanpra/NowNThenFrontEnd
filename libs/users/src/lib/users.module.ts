e0f6e8d269d810a2089255752615ac968fcde3cb
Gyan Prakash <gyanp008@gmail.com>
2021-12-08 22:01:42 +0530

    initiated ngrx with nx console

--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromUsers from './state/users.reducer';
+import { UsersEffects } from './state/users.effects';
 
 export const usersRoutes: Route[] = [
   {
-    path: 'login', component: LoginComponent}
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(usersRoutes),    FormsModule,
-    ReactiveFormsModule,],
-  declarations: [
-    LoginComponent
+  imports: [
+    CommonModule,
+    RouterModule.forChild(usersRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature(fromUsers.USERS_FEATURE_KEY, fromUsers.reducer),
+    EffectsModule.forFeature([UsersEffects]),
   ],
+  declarations: [LoginComponent],
 })
 export class UsersModule {}
