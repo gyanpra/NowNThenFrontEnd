25fc6fce89e04feff3b4956ba1562d1ddc6a4c50
Gyan Prakash <gyanp008@gmail.com>
2021-12-08 17:43:17 +0530

    category banner and productitem

--- libs/products/src/lib/components/product-item/product-item.component.ts
+++ libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { Product } from '@nownthenfrontend/products';
-import { Input } from '@angular/core';
+import { CartService } from 'libs/orders/src/lib/services/cart.service';
+import { CartItem } from 'libs/orders/src/lib/models/cart';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'products-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
+  providers: [MessageService]
 })
 export class ProductItemComponent implements OnInit {
 
-  @Input() product: Product | undefined;
+  @Input() product: Product | any;
 
-  constructor() { }
+  constructor(private cartService: CartService, private messageService:MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  addProductToCart() {
+
+    const CartItem : CartItem = {
+      productId : this.product.id,
+      quantity : 1,
+    }  
+    this.cartService.setCartItem(CartItem);
   }
 
 }
