38e8645f7260028966013d6bdb0c3d6db4a375d1
Gyan Prakash <gyanp008@gmail.com>
2021-12-08 17:41:47 +0530

    small chenges

--- libs/orders/src/index.ts
+++ libs/orders/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/orders.module';
 export * from './lib/models/order';
+export * from './lib/models/cart';
 export * from './lib/models/orderitem';
 export * from './lib/services/orders.service';
+// export * from './lib/services/cart.service';
